f0dd57992f613d243c77c48c528363fbad1b2c48
cody.coggins@perficient.com <cody.coggins@perficient.com>
2016-05-03 11:35:20 -0400

    port number change 2

--- server/node-server.js
+++ server/node-server.js
@@ -11,7 +11,7 @@ const nodeAppServer = require('./node-app-server');
  */
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Enable various security helpers.
 app.use(helmet());
