62ae21eaa879f0046a31618929aab7e1aad37157
cody.coggins@perficient.com <cody.coggins@perficient.com>
2016-05-16 18:00:20 -0400

    work in progress not debugged

--- src/services/chat-session-service.ts
+++ src/services/chat-session-service.ts
@@ -11,32 +11,59 @@ import {ChatItem} from './chat-item';
 export class ChatSessionService {
 
     http: Http;
+    headers: Headers;
+
+    conversationId: string;
+    clientId: string;
+    dialogId: string;
+
+
+    apiHostName: string =
+      'http://nielsen-bluemix-orchestration-app.mybluemix.net';
 
     constructor(http: Http)  {
         this.http = http;
+        this.headers.append('Content-Type', 'application/json; charset=utf-8');
+        this.headers.append('Accept', 'application/json');
     }
 
     getAllChats() {
-        return this.http.get('/api/chat');
+        return this.http.get('/api/conversation');
     }
 
+    initiateChat() : Observable<Response> {
+      let apiPath: string = '/api/initConversation';
+      return this.http.get( this.apiHostName + apiPath ,
+        {headers: this.headers }).share();
+
+    }
     // formerly returned Observable<List<ChatItem>>
-    addUserChatItem(newChatItem: ChatItem) : Observable<Response> {
-        const headers = new Headers();
-        const url =
-      'http://nielsen-bluemix-orchestration-app.mybluemix.net/api/conversation';
-        headers.append('Content-Type', 'application/json; charset=utf-8');
-        headers.append('Accept', 'application/json');
+    addUserChatItem(newChatItem: ChatItem) : Observable <Response> {
+
 
         const params = new URLSearchParams();
-        params.set('message', newChatItem.text);
+        params.set('input', newChatItem.text);
+
+        // if this is the first post for the application, only supply input
+        let apiPath: string =  '/api/conversation/client/'
+          + this.clientId + '/id/' + this.conversationId;
+        // if not first time, provide dialog and conversation IDs
+        if (this.clientId != null && this.conversationId != null) {
+          console.log ('GET ' + this.apiHostName + apiPath );
+          // payload should be:  JSON.stringify(newChatItem)
+          return this.http.get(this.apiHostName + apiPath ,
+            {headers: this.headers, search: params }).share();
+        }
         // post returns Observable<Response>
-        console.log ('GET ' + url + ', ' + JSON.stringify(newChatItem));
-        // payload should be:  JSON.stringify(newChatItem)
-        return this.http.get(url,
-          {headers: headers, search: params}).share();
     }
 
+    recordSessionIDs(res: Response) {
+      console.log('recordSessionIDs: ' + res.text());
+      let body: any = res.json;
+      console.log('extracted conversation_id: ' + body.conversation_id);
+      console.log('extracted clientId: ' + body.client_id);
+      console.log('extracted response: ' + JSON.stringify(body.response));
+    }
 
 
 }
