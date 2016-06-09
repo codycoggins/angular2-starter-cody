ef56089a9a8447061b130f11f9e7b8498b654c8a
Apoorv Rajput <apoorv.rajput@perficient.com>
2016-06-09 14:40:11 -0400

    more Pie changes

--- src/containers/visual-pie.ts
+++ src/containers/visual-pie.ts
@@ -1,3 +1,6 @@
+/// <reference path="../../typings/main/ambient/d3/d3.cloud.layout.d.ts" />
+
+
 import { Component, OnInit } from 'angular2/core';
 import {Observable} from 'rxjs/Observable';
 import {List} from 'immutable';
@@ -6,12 +9,13 @@ import {ChatItem} from '../services/chat-item';
 import { ChatSessionStore } from '../services/chat-session-store';
 import { OLMessage, OLProfile} from '../services/chat-session-service';
 import { ITranslatedData } from '../services/chat-session-store';
+//import * as e3 from '../../typings/main/ambient/d3/d3.cloud.layout.d.ts';
 
 // import { topojson } from 'ts-topojson';
 // import { Base } from 'Topojson';
 
 declare let g, d3, topojson: any;
-
+declare var exports: any;
 import {
   WatsonContainer
 } from '../components';
@@ -42,21 +46,29 @@ import {
     <div id="chart_div" >
     <table>
     <tr>
-
+    <td valign="top">
     <div id="pie-chart">
     <div class="visual-title">Sentiments Pie chart</div>
     </div>
+    </td>
+    <td valign="top">
+    <div class="visual-title">Sentiments Word Cloud</div>
+    <div id="word-cloud"> </div>
+    </td>
     </tr>
+    </table>
+    <table>
     <tr>
     <td valign="top">
 
-    <div id="positive-sentiments-table">
+    <div id="positive-sentiments-table" style="display: {{ positive ? 'Block' : 'None' }};">
+
     <div class="visual-title">Positive Sentiments Table</div>
     <div innerHTML="{{ dataInColumnHTML('POSITIVE') }}"></div>
     </div>
     </td>
     <td valign="top">
-    <div id="negative-sentiments-table">
+    <div id="negative-sentiments-table" style="display: {{ negative ? 'Block' : 'None' }};">
     <div class="visual-title"> Negative Sentiments Table</div>
     <div innerHTML="{{ dataInColumnHTML('NEGATIVE') }}"></div>
     </div>
@@ -72,11 +84,27 @@ import {
     data: any;
     dataRough: any;
     chatSessionStore: ChatSessionStore;
+    color: any;
+    positive: boolean = false;
+    negative:boolean = false;
 
     constructor(chatSessionStore: ChatSessionStore ) {
       console.log('VisualPie constructor() ');
       this.chatSessionStore = chatSessionStore;
+      this.color = d3.scale.linear()
+              .domain([0,1,2,3,4,5,6,10,15,20,100])
+              .range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]);
     };
+
+    setPositive(hidden: boolean) {
+      this.positive =  hidden;
+      this.negative = !hidden;
+    }
+
+    setNegative(hidden: boolean) {
+      this.negative =  hidden;
+      this.positive = !hidden;
+    }
 
     ngOnInit() {
       let intent = this.chatSessionStore.intent;
@@ -84,10 +112,75 @@ import {
       let yFunction ;
       this.dataRough =  this.chatSessionStore.translatedData();
       this.drawPieChart();
+      this.drawWordCloud();
       //this.data = this.dataRough;
       // console.log(' Data JSON ' + JSON.stringify(this.data));
     }
 
+drawWordCloud() {
+  let frequency_list = [{"text":"suave","size":50},
+{"text":"hair","size":46},
+{"text":"shampoo","size":22},
+{"text":"perfect","size":10},
+{"text":"conditioner","size":10},
+{"text":"cut","size":10},
+{"text":"long","size":7},
+{"text":"cut","size":10},
+{"text":"look","size":6},
+{"text":"pantene","size":5},
+{"text":"suaveshinyhair","size":5},
+{"text":"@shznakl","size":5},
+{"text":"ad","size":5},
+{"text":"professionals","size":5},
+{"text":"products","size":5},
+{"text":"disappointment","size":5},
+{"text":"random","size":5},
+{"text":"moroccan","size":5},
+{"text":"stupid","size":5},
+{"text":"@jvigil56","size":4},
+{"text":"smells","size":4},
+{"text":"tease","size":4},
+{"text":"@jvigil56","size":4},
+{"text":"poofy","size":4},
+{"text":"volume","size":3},
+{"text":"deal","size":3},
+{"text":"savings","size":3},
+{"text":"coupon","size":3},
+{"text":"@deshartsock","size":3},
+{"text":"wedding","size":3}];
+
+  d3.layout.cloud().size([800, 300])
+            .words(frequency_list)
+            .rotate(0)
+            .fontSize(function(d) { return d.size; })
+            .on("end", this.draw)
+            .start();
+}
+
+draw(words) {
+
+  let color: any;
+  color = d3.scale.linear()
+          .domain([0,1,2,3,4,5,6,10,15,20,100])
+          .range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]);
+   d3.select("#word-cloud").append("svg")
+           .attr("width", 850)
+           .attr("height", 350)
+           .attr("class", "wordcloud")
+           .append("g")
+           // without the transform, words words would get cutoff to the left and top, they would
+           // appear outside of the SVG area
+           .attr("transform", "translate(320,200)")
+           .selectAll("text")
+           .data(words)
+           .enter().append("text")
+           .style("font-size", function(d) { return d.size + "px"; })
+           .style("fill", function(d, i) { return color(i); })
+           .attr("transform", function(d) {
+               return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+           })
+           .text(function(d) { return d.text; });
+}
     drawPieChart() {
       console.log ('\n SOCIAL FEEDBACK \n');
       this.dataRough = this.processSocialFeedback (this.dataRough);
@@ -125,11 +218,22 @@ import {
       .attr('d', arc)
       .on("click", function(d){
         console.log(d.data.key);
+        this.positive =  true;
+        this.negative = false;
         if(d.data.key == 'Positive') {
+          this.positive =  true;
+          this.negative = false;
+              // document.getElementById('positive-sentiments-table').style.display = 'hide';
+           document.getElementById('negative-sentiments-table').style.display = 'none';
+           document.getElementById('positive-sentiments-table').style.display = 'block';
           //this.dataInColumnHTML(d.data.key);
         } else
         if(d.data.key == 'Negative') {
-          //this.dataInColumnHTML(d.data.key);
+          this.positive =  false;
+          this.negative = true;
+          //document.getElementById('negative-sentiments-table').style.display = 'hide';
+          document.getElementById('positive-sentiments-table').style.display = 'none';
+            document.getElementById('negative-sentiments-table').style.display = 'block';
         }
 
       })
